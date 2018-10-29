81fdad8b77cf70f474eb700a33488e3e545779a6
captainburnaby <bineykingsley36@gmail.com>
2018-10-29 14:53:59 +0000

    edited code

--- run.py
+++ run.py
@@ -4,19 +4,20 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route("/api/os/<int:id_number>")
 def view_results(id_number):
     # read data from excel to csv
-    try:
-        with xlrd.open_workbook("cpen_OS_Groups_to_TA.xlsx") as wb:
-             sh = wb.sheet_by_index(1)
-             with open("data.csv", "wb") as f:
-                 c = csv.writer(f)
-                 for r in range(1, sh.nrows):
-                     c.writerow(sh.row_values(r))
-    except Exception as e:
-        return jsonify({"status": "failure",
-                        "optional": "Error occurred during Data Fetching"})
+    # try:
+    #     with xlrd.open_workbook("cpen_OS_Groups_to_TA.xlsx") as wb:
+    #          sh = wb.sheet_by_index(1)
+    #          with open("data.csv", "wb") as f:
+    #              c = csv.writer(f)
+    #              for r in range(1, sh.nrows):
+    #                  c.writerow(sh.row_values(r))
+    # except Exception as e:
+    #     return jsonify({"status": "failure",
+    #                     "optional": "Error occurred during Data Fetching"})
 
     # main work
     headers = ["Student_ID_Number", "Name", "Quiz_1", "Quiz_2", "Quiz_3", "Quiz_4",
