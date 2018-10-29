9fbf592b3dcd8fc9038e544fae86c04805cf079c
captainburnaby <bineykingsley36@gmail.com>
2018-10-29 15:08:20 +0000

    separated csv update

---
+++ update_data_file.py
@@ -0,0 +1,18 @@
+import xlrd, csv
+import json
+
+def update_csv_file():
+    # read data from excel to csv
+    try:
+        with xlrd.open_workbook("cpen_OS_Groups_to_TA.xlsx") as wb:
+             sh = wb.sheet_by_index(1)
+             with open("data.csv", "wb") as f:
+                 c = csv.writer(f)
+                 for r in range(1, sh.nrows):
+                     c.writerow(sh.row_values(r))
+        return "done"
+    except Exception as e:
+        return "error"
+
+# always run on update
+# print(update_csv_file())
