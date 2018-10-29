08569131e106f992719b85c7a84bcc224150b370
captainburnaby <bineykingsley36@gmail.com>
2018-10-29 15:59:11 +0000

    edited run.py

--- run.py
+++ run.py
@@ -32,8 +32,8 @@ def view_results(id_number):
         results = data.loc[data["Student_ID_Number"] == id_number]
         results = (results.to_dict(orient="records"))[0]
         return jsonify({"results": results,
-                        "status": "sucess",
-                        "optional": ""})
+                        "status": "success",
+                        "optional": "All Quizes are over 10, Labs 1 and 2 are over 5 and Lab 3 is over 20. Presentation is over 10."})
     except Exception as e:
         return jsonify({"status": "failure",
                         "optional": "Unknown Error during processing.."})
